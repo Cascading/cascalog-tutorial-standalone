45b4163fb6d847bb14698b7a249ff77fb61fc5a3
Andre Kelpe <efeshundertelf@googlemail.com>
2013-07-02 17:50:00 +0200

    general cleanup, set license correctly, document the leiningen project better

--- project.clj
+++ project.clj
@@ -1,20 +1,39 @@
-(defproject cascalog-playground "1.10.1"
+;;
+;; Copyright (c) 2007-2013 Concurrent, Inc. All Rights Reserved.
+;;
+;; Project and contact information: http://www.cascading.org/
+;;
+;; This file is part of the Cascading project.
+;;
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+;;
+;;     http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+
+(defproject cascalog-standalone-tutorial "1.10.1"
   :description "The cascalog tutorial"
   :url "http://cascalog.org/articles/getting_started.html"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache license version 2"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :repositories {"conjars" "http://conjars.org/repo/"}
 
   :dependencies [[org.clojure/clojure "1.5.1"] 
                  [cascalog/cascalog "1.10.1"]
-                 ; version range pulling in the latest 2.2-wip build
+                 ; version range pulling in the latest 2.2 build
                  [cascading/cascading-hadoop "[2.2.0,2.3.0)" ]
                  ]
   :profiles {:provided
              {:dependencies
               [[org.apache.hadoop/hadoop-core "1.1.2"]]}} 
-  :plugins [[lein-libdir "0.1.1"]
-            [lein-tar "2.0.0"] 
-           ]
+  ; the libdir plugin creates a directory with all dependencies, so that we can
+  ; run fully offline
+  :plugins [[lein-libdir "0.1.1"]]
   :libdir-path "lib")
 
